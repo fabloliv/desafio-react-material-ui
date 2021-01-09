0011cda2c89328711bcb1c7a1a11a9ce2e22f644
Fabrício Oliveira <fabriciololiveira@gmail.com>
2021-01-09 17:38:18 -0300

    Organize page with country list

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,39 +1,30 @@
-import {
-  Grid,
-  Card,
-  CardMedia,
-  CardContent,
-  CircularProgress,
-  Typography,
-  CardActionArea,
-} from "@material-ui/core";
+import { CircularProgress, CssBaseline } from "@material-ui/core";
 
-import { makeStyles } from "@material-ui/core/styles";
 import { GET_ALL_COUNTRIES } from "../graphql";
 
 import { useQuery } from "@apollo/client";
+
 import Header from "./Header";
 import Footer from "./Footer";
+import CountryList from "./CountryList";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  countriesContainer: {
-    paddingTop: "20px",
-    paddingLeft: "40px",
-    paddingRight: "40px",
-  },
-  cardMedia: {
-    margin: "auto",
-  },
-  cardContent: {
-    textAlign: "center",
-  },
-  capital: {
-    fontSize: 14,
-  },
-});
+/*
+const LinkList = () => {
+  const { data } = useQuery(FEED_QUERY);
+
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link) => (
+            <Link key={link.id} link={link} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+*/
 
 const Countries = (props) => {
   const { history } = props;
@@ -41,56 +32,21 @@ const Countries = (props) => {
   // test
   let first = 15;
 
-  const classes = useStyles();
-
   const { loading, error, data } = useQuery(GET_ALL_COUNTRIES, {
     variables: { first },
   });
 
   if (loading) return <CircularProgress />;
-  if (error) return `Error! ${error.message}`;
+  if (error) return `Erro! ${error.message}`;
 
   return (
     <>
-      <Header title="Países" />
+      <CssBaseline />
+      <Header title="Graph Countries" />
 
-      {/* gera lista de países */}
-      {/* <ListCountries data={data} /> */}
+      {/* gera lista de países | passa history como prop */}
+      <CountryList data={data} history={history} />
 
-      <Grid
-        container
-        spacing={2}
-        direction="row"
-        justify="center"
-        alignItems="center"
-        className={classes.countriesContainer}
-      >
-        {data?.Country?.map((country) => (
-          <Grid item xs={12} sm={6} md={3} key={country._id}>
-            <Card className={classes.root}>
-              <CardActionArea onClick={() => history.push(`/${country._id}`)}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  component="img"
-                  alt={`Bandeira de ${country.nameTranslations[0].value}`}
-                  height="140"
-                  image={country.flag.svgFile}
-                  title={country.name}
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom={false} variant="h6" component="h2">
-                    {country.nameTranslations[0].value}
-                  </Typography>
-
-                  <Typography className={classes.capital} color="textSecondary">
-                    {country.capital}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
       <Footer />
     </>
   );
